0009c8d1d8a31fc17dae140be459efb4a923e88e
CERTIFIEDBANANAA <skwa210556435@gmail.com>
2024-07-18 21:08:30 -0700

    Update dllscripts.js

--- dllscripts.js
+++ dllscripts.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const files = [
-        { name: "BananaMenu", size: "35 KB", type: "file", link:"Banana Menu 1.0.0.dll"}
+        { name: "  BananaMenu", size: "35 KB", type: "file", link:"Banana Menu 1.0.0.dll"}
     ];
 
     const fileList = document.getElementById("file-list");
