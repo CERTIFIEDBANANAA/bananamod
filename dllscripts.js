de2774da6caf55800499f7f1e3dc54e94e9cd790
CERTIFIEDBANANAA <skwa210556435@gmail.com>
2024-07-18 21:05:10 -0700

    Update dllscripts.js

--- dllscripts.js
+++ dllscripts.js
@@ -1,1 +1,35 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+    const files = [
+        { name: "dlls", size: "", type: "folder", link:"/dlls"},
+        { name: "raw_csharp", size: "", type: "folder", link: "/raw"}
+    ];
+
+    const fileList = document.getElementById("file-list");
+
+    files.forEach(file => {
+        const li = document.createElement("li");
+
+        const icon = document.createElement("span");
+        const a = document.createElement("a");
+        a.href=file.link
+      
+        icon.classList.add("icon");
+        icon.innerHTML = file.type === "folder" ? "📁" : "📄";
+        
+        const name = document.createElement("span");
+        name.classList.add("file-name");
+        name.textContent = file.name;
+
+        const size = document.createElement("span");
+        size.classList.add("file-size");
+        size.textContent = file.size;
+
+        li.appendChild(icon);
+        li.appendChild(size);
+        li.appendChild(a)
+        a.appendChild(name);
+
+        fileList.appendChild(li);
+    });
+});
